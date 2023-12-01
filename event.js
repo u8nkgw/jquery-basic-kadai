86c08a22881e672476bc3d2edf1fad4e2c54499a
u8nkgw <un44448699@gmail.com>
2023-12-01 21:20:33 +0900

    課題004

---
+++ event.js
@@ -0,0 +1,10 @@
+
+  // 読み込んだ際
+  $(window).on('load', function(){
+    console.log('loadイベントが発生しました');
+  });
+
+  // スクロールした際
+  $(window).on('scroll', function(){
+    console.log('lscrollイベントが発生しました');
+  });
